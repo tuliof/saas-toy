3c6b4e64ca8eedf4ac123d27523a3979b3d8b0b7
Tulio Fernandes <tulio.fernandes@gmail.com>
2025-01-21 12:46:51 -0500

    first commit

--- middleware.ts
+++ middleware.ts
@@ -15,28 +15,30 @@ export async function middleware(request: NextRequest) {
 
   let res = NextResponse.next();
 
-  if (sessionCookie) {
-    try {
-      const parsed = await verifyToken(sessionCookie.value);
-      const expiresInOneDay = new Date(Date.now() + 24 * 60 * 60 * 1000);
+  if (!sessionCookie) {
+    return res;
+  }
 
-      res.cookies.set({
-        name: 'session',
-        value: await signToken({
-          ...parsed,
-          expires: expiresInOneDay.toISOString(),
-        }),
-        httpOnly: true,
-        secure: true,
-        sameSite: 'lax',
-        expires: expiresInOneDay,
-      });
-    } catch (error) {
-      console.error('Error updating session:', error);
-      res.cookies.delete('session');
-      if (isProtectedRoute) {
-        return NextResponse.redirect(new URL('/sign-in', request.url));
-      }
+  try {
+    const parsed = await verifyToken(sessionCookie.value);
+    const expiresInOneDay = new Date(Date.now() + 24 * 60 * 60 * 1000);
+
+    res.cookies.set({
+      name: 'session',
+      value: await signToken({
+        ...parsed,
+        expires: expiresInOneDay.toISOString(),
+      }),
+      httpOnly: true,
+      secure: true,
+      sameSite: 'lax',
+      expires: expiresInOneDay,
+    });
+  } catch (error) {
+    console.error('Error updating session:', error);
+    res.cookies.delete('session');
+    if (isProtectedRoute) {
+      return NextResponse.redirect(new URL('/sign-in', request.url));
     }
   }
 
